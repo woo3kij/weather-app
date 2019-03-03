ee6a47e359ccb84627427592de99af0bdbb972b1
woo3kij <woo3kij@gmail.com>
2019-03-03 21:47:51 +0100

    build of components

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('index');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
